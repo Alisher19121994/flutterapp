d400f7aca329d34403545ad5d990793d174f561b
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-02-19 21:45:41 +0500

    Hello Flutter

---
+++ lib/pages/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:ios/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+      routes:{
+        HomePage.id: (context)=> HomePage()
+      }
+
+    );
+  }
+}
+
